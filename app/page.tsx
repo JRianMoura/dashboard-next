7b856d0ccd6f1e61ba760c99cd183550ecb0882f
Judson Rian <judson_@outlook.com>
2024-07-03 15:59:59 -0300

    correções

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Bem vindo</strong> Este é o{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              RianBank
-            </a>
+            Bem vindo este é o{' '}
+            <span className="text-blue-500">
+              <strong>RianBank</strong>
+            </span>
             , feito para você.
           </p>
           <Link
