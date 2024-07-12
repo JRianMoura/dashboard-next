1f862e560ccb1ec0f23bd37e9cdcb844c7ec66a3
Judson Rian <judson_@outlook.com>
2024-07-12 18:46:38 -0300

    update

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,28 @@
+import Form from "@/app/ui/invoices/edit-form"
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs"
+import { fetchCustomers, fetchInvoiceById} from "@/app/lib/data"
+
+
+const page = async ({params}:{params: {id: string} }) => {
+  const id = params.id;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs breadcrumbs={[
+        { label: 'Invoices', href: '/dashboard/invoices' },
+        {
+          label: 'Editar Faturas', href: `/dashboard/invoices/${id}/edit`,
+      active: true  }
+      ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  )
+}
+
+export default page
